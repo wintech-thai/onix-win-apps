c569e46996519dd217595ec495d0b9f66cb951b9
pjames <pjames@gmail.com>
2024-08-04 18:53:30 +0700

    Added logic to check program version from build.txt

--- onix_client/OnixClientDll/Helper/CBuild.cs
+++ onix_client/OnixClientDll/Helper/CBuild.cs
@@ -2,12 +2,33 @@
 * This is auto generated class, DO NOT modify
 */
 using System;
+using System.IO;
 
 namespace Onix.Client.Helper
 {
    public static class CBuild
    {
-        public static String BuildVersion = "<version-here>";
+        public static String BuildVersion = readAppVersion();
         public static String Product = "onix";
+
+        public static string readAppVersion()
+        {
+            var version = "<version-here>";
+
+            try
+            {
+                var lines = File.ReadAllLines("build.txt");
+                if (lines.Length > 0)
+                {
+                    version = lines[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                //Do nothing
+            }
+
+            return version;
+        }
     }
 }
